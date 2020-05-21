1d30b0977d3dfb1343daa4e479ba9e295d0c1ef1
tupurpcheung <tupurpchueng@gmail.com>
2020-05-21 18:10:21 +0800

    springboot init

---
+++ src/main/java/com/tupurp/learn/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tupurp.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
